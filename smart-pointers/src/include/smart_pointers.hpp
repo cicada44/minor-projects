6dfa984980e4a1def265ac2642ddea83cccf5132
Cicada <hubbleadd@bk.ru>
2023-07-17 23:06:08 +0700

    Aff smart-pointers project

--- smart-pointers/src/include/smart_pointers.hpp
+++ smart-pointers/src/include/smart_pointers.hpp
@@ -14,60 +14,433 @@ namespace smart_pointers {
 template <typename T, typename Deleter = std::default_delete<T>>
 class unique_ptr {
 public:
-    unique_ptr(T& obj) : _object(&obj) {}
-    unique_ptr(T* object = nullptr) : _object(object) {}
+    /**
+     * @brief Constructs a unique_ptr that manages the given object.
+     *
+     * @param obj The object to manage.
+     */
+    unique_ptr(T& obj);
 
-    /* Make the unique_ptr unique_ptr! */
-    unique_ptr(const T& unique_ptr) = delete;
-    unique_ptr operator=(const T& unique_ptr) = delete;
+    /**
+     * @brief Constructs a unique_ptr that manages the given object pointer.
+     *
+     * @param object The object pointer to manage.
+     */
+    unique_ptr(T* object = nullptr);
 
-    ~unique_ptr() { reset(); }
+    // Make the unique_ptr non-copyable
+    unique_ptr(const unique_ptr&) = delete;
+    unique_ptr operator=(const unique_ptr&) = delete;
 
-    unique_ptr(unique_ptr&& other) noexcept : _object(other._object) {
-        other._object = nullptr;
-    }
+    /**
+     * @brief Moves the ownership of the managed object from 'other' to the
+     * current unique_ptr.
+     *
+     * @param other The unique_ptr to move from.
+     */
+    unique_ptr(unique_ptr&& other) noexcept;
 
-    unique_ptr& operator=(unique_ptr&& other) noexcept {
-        if (_object != other._object) {
-            reset(other._object);
-            other._object = nullptr;
-        }
+    /**
+     * @brief Moves the ownership of the managed object from 'other' to the
+     * current unique_ptr.
+     *
+     * @param other The unique_ptr to move from.
+     * @return A reference to the current unique_ptr.
+     */
+    unique_ptr& operator=(unique_ptr&& other) noexcept;
 
-        return *this;
-    }
+    /**
+     * @brief Resets the unique_ptr to manage the new object pointer.
+     *
+     * @param object The new object pointer to manage.
+     */
+    void reset(T* object = nullptr);
 
-    void reset(T* object = nullptr) {
-        _deleter(_object);
-        _object = object;
-    }
+    /**
+     * @brief Dereferences the managed object.
+     *
+     * @return A reference to the managed object.
+     */
+    T& operator*() const;
 
-    T& operator*() const { return *_object; }
+    /**
+     * @brief Returns the managed object pointer.
+     *
+     * @return The managed object pointer.
+     */
+    T* operator->() const;
 
-    T* operator->() const { return _object; }
+    /**
+     * @brief Releases the ownership of the managed object and returns the
+     * object pointer.
+     *
+     * @return The object pointer.
+     */
+    T* release();
 
-    T* release() {
-        T* releasedObject = _object;
-        _object = nullptr;
-        return releasedObject;
-    }
+    /**
+     * @brief Returns the managed object pointer.
+     *
+     * @return The managed object pointer.
+     */
+    T* get() const;
 
-    T* get() const { return _object; }
+    /**
+     * @brief Checks if the unique_ptr is not null.
+     *
+     * @return 'true' if the unique_ptr is not null, 'false' otherwise.
+     */
+    explicit operator bool() const;
 
-    explicit operator bool() const { return _object != nullptr; }
+    /**
+     * @brief Resets the unique_ptr to manage a null object pointer.
+     *
+     * @return A reference to the current unique_ptr.
+     */
+    unique_ptr& operator=(std::nullptr_t);
 
-    unique_ptr& operator=(std::nullptr_t) {
-        reset();
-        return *this;
-    }
+    /**
+     * @brief Destructor that releases the ownership of the managed object.
+     */
+    ~unique_ptr();
 
 private:
     T* _object;
     Deleter _deleter;
 };
 
+template <typename T, typename Deleter>
+unique_ptr<T, Deleter>::unique_ptr(T& obj) : _object(&obj) {}
+
+template <typename T, typename Deleter>
+unique_ptr<T, Deleter>::~unique_ptr() {
+    reset();
+}
+
+template <typename T, typename Deleter>
+unique_ptr<T, Deleter>::unique_ptr(T* object) : _object(object) {}
+
+template <typename T, typename Deleter>
+unique_ptr<T, Deleter>::unique_ptr(unique_ptr&& other) noexcept
+    : _object(other._object) {
+    other._object = nullptr;
+}
+
+template <typename T, typename Deleter>
+unique_ptr<T, Deleter>& unique_ptr<T, Deleter>::operator=(
+    unique_ptr&& other) noexcept {
+    if (_object != other._object) {
+        reset(other._object);
+        other._object = nullptr;
+    }
+
+    return *this;
+}
+
+template <typename T, typename Deleter>
+void unique_ptr<T, Deleter>::reset(T* object) {
+    _deleter(_object);
+    _object = object;
+}
+
+template <typename T, typename Deleter>
+T& unique_ptr<T, Deleter>::operator*() const {
+    return *_object;
+}
+
+template <typename T, typename Deleter>
+T* unique_ptr<T, Deleter>::operator->() const {
+    return _object;
+}
+
+template <typename T, typename Deleter>
+T* unique_ptr<T, Deleter>::release() {
+    T* releasedObject = _object;
+    _object = nullptr;
+    return releasedObject;
+}
+
+template <typename T, typename Deleter>
+T* unique_ptr<T, Deleter>::get() const {
+    return _object;
+}
+
+template <typename T, typename Deleter>
+unique_ptr<T, Deleter>::operator bool() const {
+    return _object != nullptr;
+}
+
+template <typename T, typename Deleter>
+unique_ptr<T, Deleter>& unique_ptr<T, Deleter>::operator=(std::nullptr_t) {
+    reset();
+    return *this;
+}
+
 template <typename T, typename... Args>
 unique_ptr<T> make_unique(Args&&... args) {
     return unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+/**
+ * @brief Constructs a unique_ptr that manages a new object created with the
+ * provided arguments.
+ *
+ * @tparam T The type of the object to create.
+ * @tparam Args The types of the arguments to forward to the object constructor.
+ * @param args The arguments to forward to the object constructor.
+ * @return A unique_ptr that manages the created object.
+ */
+template <typename T, typename... Args>
+unique_ptr<T> make_unique(Args&&... args);
+
+template <typename T, typename Deleter = std::default_delete<T>>
+class shared_ptr {
+public:
+    /**
+     * @brief Constructs a shared_ptr that manages the given object pointer.
+     *
+     * @param object The object pointer to manage.
+     */
+    shared_ptr(T* object = nullptr);
+
+    /**
+     * @brief Constructs a shared_ptr that shares ownership with the given
+     * shared_ptr.
+     *
+     * @param ptr The shared_ptr to share ownership with.
+     */
+    shared_ptr(const shared_ptr<T>& ptr);
+
+    /**
+     * @brief Assigns the ownership of the managed object from another
+     * shared_ptr.
+     *
+     * @param object The shared_ptr to assign ownership from.
+     * @return A reference to the current shared_ptr.
+     */
+    shared_ptr& operator=(const shared_ptr<T>& object);
+
+    /**
+     * @brief Moves the ownership of the managed object from 'other' to the
+     * current shared_ptr.
+     *
+     * @param other The shared_ptr to move from.
+     */
+    shared_ptr(shared_ptr&& other) noexcept;
+
+    /**
+     * @brief Dectructs the object if there're no any owners.
+     */
+    ~shared_ptr();
+
+    /**
+     * @brief Moves the ownership of the managed object from 'other' to the
+     * current shared_ptr.
+     *
+     * @param other The shared_ptr to move from.
+     * @return A reference to the current shared_ptr.
+     */
+    shared_ptr& operator=(shared_ptr&& other) noexcept;
+
+    /**
+     * @brief Resets the shared_ptr to manage the new object pointer.
+     *
+     * @param new_object The new object pointer to manage.
+     */
+    void reset(T* new_object = nullptr);
+
+    /**
+     * @brief Returns the managed object pointer.
+     *
+     * @return The managed object pointer.
+     */
+    T* get() const;
+
+    /**
+     * @brief Dereferences the managed object.
+     *
+     * @return A reference to the managed object.
+     */
+    T& operator*() const;
+
+    /**
+     * @brief Returns the managed object pointer.
+     *
+     * @return The managed object pointer.
+     */
+    T* operator->() const;
+
+    /**
+     * @brief Returns the number of shared_ptr instances sharing the ownership
+     * of the managed object.
+     *
+     * @return The number of shared_ptr instances sharing the ownership.
+     */
+    size_t use_count() const;
+
+    /**
+     * @brief Checks if the shared_ptr is the sole owner of the managed object.
+     *
+     * @return 'true' if the shared_ptr is the sole owner, 'false' otherwise.
+     */
+    bool unique() const;
+
+    /**
+     * @brief Checks if the shared_ptr manages a non-null object pointer.
+     *
+     * @return 'true' if the shared_ptr manages a non-null object pointer,
+     * 'false' otherwise.
+     */
+    explicit operator bool() const;
+
+private:
+    T* _object;
+    Deleter _deleter;
+    size_t* _refcnt;
+
+    /**
+     * @brief Releases the ownership of the managed object and deallocates it if
+     * necessary.
+     */
+    void release();
+
+    /**
+     *
+     * @brief Resets the shared_ptr to manage the new object pointer and updates
+     * the reference count pointer.
+     *
+     * @param new_object The new object pointer to manage.
+     * @param new_refcnt The new reference count pointer to update.
+     */
+    void reset_internal(T* new_object, size_t* new_refcnt);
+};
+
+/**
+ * @brief Constructs a shared_ptr that manages a new object created with the
+ * provided arguments.
+ *
+ * @tparam T The type of the object to create.
+ * @tparam Args The types of the arguments to forward to the object constructor.
+ * @param args The arguments to forward to the object constructor.
+ * @return A shared_ptr that manages the created object.
+ */
+template <typename T, typename... Args>
+shared_ptr<T> make_shared(Args&&... args);
+
+template <typename T, typename Deleter>
+shared_ptr<T, Deleter>::shared_ptr(T* object)
+    : _object(object), _refcnt(nullptr) {
+    if (_object != nullptr) {
+        _refcnt = new size_t(1);
+    }
+}
+
+template <typename T, typename Deleter>
+shared_ptr<T, Deleter>::shared_ptr(const shared_ptr<T>& ptr)
+    : _object(ptr._object), _refcnt(ptr._refcnt) {
+    if (_object != nullptr) {
+        ++(*_refcnt);
+    }
+}
+
+template <typename T, typename Deleter>
+shared_ptr<T, Deleter>& shared_ptr<T, Deleter>::operator=(
+    const shared_ptr<T>& object) {
+    if (_object != object._object) {
+        reset_internal(object._object, object._refcnt);
+        if (_refcnt != nullptr) {
+            ++(*_refcnt);
+        }
+    }
+    return *this;
+}
+
+template <typename T, typename Deleter>
+shared_ptr<T, Deleter>::shared_ptr(shared_ptr&& other) noexcept
+    : _object(other._object), _refcnt(other._refcnt) {
+    other._object = nullptr;
+    other._refcnt = nullptr;
+}
+
+template <typename T, typename Deleter>
+shared_ptr<T, Deleter>& shared_ptr<T, Deleter>::operator=(
+    shared_ptr&& other) noexcept {
+    if (this != &other) {
+        reset_internal(other._object, other._refcnt);
+        other._object = nullptr;
+        other._refcnt = nullptr;
+    }
+    return *this;
+}
+
+template <typename T, typename Deleter>
+void shared_ptr<T, Deleter>::reset_internal(T* new_object, size_t* new_refcnt) {
+    release();
+    _object = new_object;
+    _refcnt = new_refcnt;
+}
+
+template <typename T, typename Deleter>
+shared_ptr<T, Deleter>::~shared_ptr() {
+    release();
+}
+
+template <typename T, typename Deleter>
+void shared_ptr<T, Deleter>::reset(T* new_object) {
+    _deleter(_object);
+    _object = new_object;
+    if (new_object != nullptr) {
+        *_refcnt = 1;
+    } else {
+        *_refcnt = 0;
+    }
+}
+
+template <typename T, typename Deleter>
+T* shared_ptr<T, Deleter>::get() const {
+    return _object;
+}
+
+template <typename T, typename Deleter>
+T& shared_ptr<T, Deleter>::operator*() const {
+    return *_object;
+}
+
+template <typename T, typename Deleter>
+T* shared_ptr<T, Deleter>::operator->() const {
+    return _object;
+}
+
+template <typename T, typename Deleter>
+size_t shared_ptr<T, Deleter>::use_count() const {
+    return *_refcnt;
+}
+
+template <typename T, typename Deleter>
+bool shared_ptr<T, Deleter>::unique() const {
+    return *_refcnt == 1;
+}
+
+template <typename T, typename Deleter>
+shared_ptr<T, Deleter>::operator bool() const {
+    return _object != nullptr;
+}
+
+template <typename T, typename Deleter>
+void shared_ptr<T, Deleter>::release() {
+    if (_refcnt != nullptr) {
+        if (--(*_refcnt) == 0) {
+            delete _refcnt;
+            _deleter(_object);
+            _object = nullptr;
+            _refcnt = nullptr;
+        }
+    }
+}
+
+template <typename T, typename... Args>
+shared_ptr<T> make_shared(Args&&... args) {
+    return shared_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 }  // namespace smart_pointers
--- smart-pointers/test/test/test.cpp
+++ smart-pointers/test/test/test.cpp
@@ -76,6 +76,115 @@ TEST(UniquePtr, ArrowOperator) {
     ASSERT_EQ(ptr->value, 42);
 }
 
+TEST(SharedPtr, Constructors) {
+    // Default constructor
+    {
+        smart_pointers::shared_ptr<int> intptr;
+        ASSERT_FALSE(intptr.get());
+    }
+
+    // Constructor with raw pointer
+    {
+        int* rawPtr = new int(42);
+        smart_pointers::shared_ptr<int> intptr(rawPtr);
+
+        ASSERT_TRUE(intptr.get());
+        ASSERT_EQ(*(intptr.get()), 42);
+        ASSERT_EQ(intptr.use_count(), 1);
+        ASSERT_TRUE(intptr.unique());
+    }
+
+    // Copy constructor
+    {
+        int* rawPtr = new int(42);
+        smart_pointers::shared_ptr<int> intptr(rawPtr);
+        smart_pointers::shared_ptr<int> copyPtr(intptr);
+
+        ASSERT_EQ(intptr.get(), copyPtr.get());
+        ASSERT_EQ(intptr.use_count(), 2);
+        ASSERT_FALSE(intptr.unique());
+        ASSERT_FALSE(copyPtr.unique());
+    }
+
+    // Move constructor
+    {
+        int* rawPtr = new int(42);
+        smart_pointers::shared_ptr<int> intptr(rawPtr);
+        smart_pointers::shared_ptr<int> movedPtr(std::move(intptr));
+
+        ASSERT_FALSE(intptr.get());
+        ASSERT_TRUE(movedPtr.get());
+        ASSERT_EQ(*(movedPtr.get()), 42);
+        ASSERT_EQ(movedPtr.use_count(), 1);
+        ASSERT_TRUE(movedPtr.unique());
+    }
+}
+
+TEST(SharedPtr, AssignmentOperators) {
+    // Copy assignment operator
+    {
+        int* rawPtr = new int(42);
+        smart_pointers::shared_ptr<int> intptr(rawPtr);
+        smart_pointers::shared_ptr<int> copyPtr;
+        copyPtr = intptr;
+
+        ASSERT_EQ(intptr.get(), copyPtr.get());
+        ASSERT_EQ(intptr.use_count(), 2);
+        ASSERT_FALSE(intptr.unique());
+        ASSERT_FALSE(copyPtr.unique());
+    }
+
+    // Move assignment operator
+    {
+        int* rawPtr1 = new int(42);
+        smart_pointers::shared_ptr<int> intptr1(rawPtr1);
+
+        int* rawPtr2 = new int(10);
+        smart_pointers::shared_ptr<int> intptr2(rawPtr2);
+        intptr1 = std::move(intptr2);
+
+        ASSERT_FALSE(intptr2.get());
+        ASSERT_TRUE(intptr1.get());
+        ASSERT_EQ(*(intptr1.get()), 10);
+        ASSERT_EQ(intptr1.use_count(), 1);
+        ASSERT_TRUE(intptr1.unique());
+    }
+}
+
+TEST(SharedPtr, Reset) {
+    int* rawPtr = new int(42);
+    smart_pointers::shared_ptr<int> intptr(rawPtr);
+
+    intptr.reset();
+
+    ASSERT_TRUE(intptr.get() == nullptr);
+    ASSERT_FALSE(intptr.unique());
+    ASSERT_EQ(intptr.use_count(), 0);
+}
+
+TEST(SharedPtr, ArrowOperator) {
+    struct MyStruct {
+        int value;
+        void setValue(int val) { value = val; }
+    };
+
+    smart_pointers::shared_ptr<MyStruct> ptr =
+        smart_pointers::make_shared<MyStruct>();
+
+    ptr->setValue(42);
+
+    ASSERT_EQ(ptr->value, 42);
+}
+
+TEST(SharedPtr, UseCount) {
+    int* rawPtr = new int(42);
+    smart_pointers::shared_ptr<int> intptr1(rawPtr);
+    smart_pointers::shared_ptr<int> intptr2(intptr1);
+
+    ASSERT_EQ(intptr1.use_count(), 2);
+    ASSERT_EQ(intptr2.use_count(), 2);
+}
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
